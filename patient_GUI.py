2fa49612e61b95f8636d0d4678d03e8cd47a3d44
Phoebe Dijour <phoebe.dijour@duke.edu>
2021-11-27 16:37:10 -0500

    Added loading and resizing images

--- patient_GUI.py
+++ patient_GUI.py
@@ -4,6 +4,16 @@ import tkinter as tk
 from tkinter import ttk, filedialog, messagebox, StringVar
 from PIL import Image, ImageTk
 
+def load_and_resize_image(filename):
+    pil_image = Image.open(filename)
+    original_size = pil_image.size
+    adj_factor = 0.5
+    new_width = round(original_size[0] * adj_factor)
+    new_height = round(original_size[1] * adj_factor)
+    resized_image = pil_image.resize((new_width, new_height))
+    tk_image = ImageTk.PhotoImage(resized_image)
+    return tk_image
+
 
 def design_window():
 
@@ -24,6 +34,17 @@ def design_window():
     record_data = tk.StringVar()
     record_entry_box = ttk.Entry(root, textvariable=record_data)
     record_entry_box.grid(column=1, row=1, sticky='w', padx=20, pady=20)
+
+    ttk.Label(root, text="Medical Image")\
+        .grid(column=0, row=2, sticky='w', padx=20, pady=20)
+
+    tk_medical_image = load_and_resize_image("images/acl1.jpg")
+    medical_image_label = ttk.Label(root, image=tk_medical_image)
+    medical_image_label.grid(column=0, row=3, columnspan=2, padx=20, pady=20)
+
+    tk_ecg_image = load_and_resize_image("images/test_ECG.jpg")
+    ecg_image_label = ttk.Label(root, image=tk_ecg_image)
+    ecg_image_label.grid(column=3, row=3, columnspan=2, padx=20, pady=20)
 
     root.mainloop()
 
