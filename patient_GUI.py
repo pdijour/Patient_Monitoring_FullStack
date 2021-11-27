db06a58c2d08535e8588f8dd9587bf43bf71a0a9
Phoebe Dijour <phoebe.dijour@duke.edu>
2021-11-27 16:35:46 -0500

    Added basic entry fields

--- patient_GUI.py
+++ patient_GUI.py
@@ -11,6 +11,20 @@ def design_window():
     root.configure(background="#ececec")
     root.title("Patient GUI")
 
+    ttk.Label(root, text="Patient Name") \
+        .grid(column=0, row=0, sticky='w', padx=20, pady=20)
+
+    name_data = tk.StringVar()
+    name_entry_box = ttk.Entry(root, textvariable=name_data)
+    name_entry_box.grid(column=1, row=0, sticky='w', padx=20, pady=20)
+
+    ttk.Label(root, text="Patient Medical Record Number")\
+        .grid(column=0, row=1, sticky='w', padx=20, pady=20)
+
+    record_data = tk.StringVar()
+    record_entry_box = ttk.Entry(root, textvariable=record_data)
+    record_entry_box.grid(column=1, row=1, sticky='w', padx=20, pady=20)
+
     root.mainloop()
 
 
