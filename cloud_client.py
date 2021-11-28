8da0514c7654694a8c1f183fdb79d06747862f09
Phoebe Dijour <phoebe.dijour@duke.edu>
2021-11-28 07:13:37 -0500

    Added get requests for server is on in client

--- cloud_client.py
+++ cloud_client.py
@@ -1,3 +1,11 @@
 # cloud_client.py
 
 import requests
+
+server = "http://127.0.0.1:5000"
+
+def main():
+    # Check if server running
+    r = requests.get(server + "/")
+    print(r.status_code)
+    print(r.text)
--- test_cloud_server.py
+++ test_cloud_server.py
@@ -1,1 +1,5 @@
 # test_cloud_server.py
+
+from cloud_server import initialize_server
+
+initialize_server()
