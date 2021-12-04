e5fc02894596f4847bd27a401cb0067879239db4
Phoebe Dijour <phoebe.dijour@duke.edu>
2021-12-04 17:47:34 -0500

    Changed mongo database name to mine so I can work on code

--- cloud_server.py
+++ cloud_server.py
@@ -20,8 +20,8 @@ def initialize_server():
     logging.basicConfig(filename="patient_record_server.log",
                         level=logging.DEBUG)
     print("Connecting to MongoDB...")
-    connect("mongodb+srv://mqt3:71IhMxzWnTpAhRkg@bme547.qubox.mongodb.net/"
-            "myFirstDatabase?retryWrites=true&w=majority")
+    connect("mongodb+srv://pdijour:mongopassword@bme547.qubox.mongodb.net/"
+            "final_project?retryWrites=true&w=majority")
     print("Connection attempt finished.")
 
 
@@ -264,27 +264,26 @@ def read_file_as_b64(image_path):
 
 if __name__ == '__main__':
     initialize_server()
-    path = "/Users/michael.tian/Desktop/BME 547/class_repos" \
-           "/final-project-spooky-dookie/images/"
-    images = ["acl1.jpg", "acl2.jpg", "esophagus 1.jpg", "esophagus2.jpg",
-              "synpic50411.jpg", "synpic51041.jpg", "synpic51042.jpg",
-              "upj1.jpg", "upj2.jpg", "test_ECG.jpg", "test_tachycardia.jpg"]
-    full_path = []
-    for i in range(len(images)):
-        full_path.append(path + images[i])
-    b64_images = []
-    for i in full_path:
-        b64_images.append(read_file_as_b64(i))
-    datetimes = ["2020-03-00 11:00:36", "2020-03-01 11:00:36",
-                 "2020-03-02 11:00:36"]
-    patient1 = Patient("Yume Choi", 3, ["acl1.png"], [b64_images[0]],
-                       [images[9]], [b64_images[-2]], [85], [datetimes[0]])
-    patient2 = Patient("Michael Tian", 5, images[0:9], b64_images[0:9],
-                       images[9:], b64_images[10:], [85, 90], datetimes[1:])
-    patient3 = Patient("Phoebe Dijour", 11, ["acl1.png", "acl2.png"],
-                       [b64_images[0], b64_images[1]], [images[10]],
-                       [b64_images[-1]], [85], [datetimes[0]])
-    patient1.save()
-    patient2.save()
-    patient3.save()
+    # path = "images/"
+    # images = ["acl1.jpg", "acl2.jpg", "esophagus 1.jpg", "esophagus2.jpg",
+    #           "synpic50411.jpg", "synpic51041.jpg", "synpic51042.jpg",
+    #           "upj1.jpg", "upj2.jpg", "test_ECG.jpg", "test_tachycardia.jpg"]
+    # full_path = []
+    # for i in range(len(images)):
+    #     full_path.append(path + images[i])
+    # b64_images = []
+    # for i in full_path:
+    #     b64_images.append(read_file_as_b64(i))
+    # datetimes = ["2020-03-00 11:00:36", "2020-03-01 11:00:36",
+    #              "2020-03-02 11:00:36"]
+    # patient1 = Patient("Yume Choi", 3, ["acl1.png"], [b64_images[0]],
+    #                    [images[9]], [b64_images[-2]], [85], [datetimes[0]])
+    # patient2 = Patient("Michael Tian", 5, images[0:9], b64_images[0:9],
+    #                    images[9:], b64_images[10:], [85, 90], datetimes[1:])
+    # patient3 = Patient("Phoebe Dijour", 11, ["acl1.png", "acl2.png"],
+    #                    [b64_images[0], b64_images[1]], [images[10]],
+    #                    [b64_images[-1]], [85], [datetimes[0]])
+    # patient1.save()
+    # patient2.save()
+    # patient3.save()
     app.run()
