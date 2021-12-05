be651c3af3cdf2b1fd68042bc4e87a5444074203
Michael Tian <Michaeltian77@gmail.com>
2021-12-05 01:38:59 -0500

    Commented out code used for testing

--- cloud_server.py
+++ cloud_server.py
@@ -300,28 +300,28 @@ def resize_image(nd):
 if __name__ == '__main__':
     initialize_server()
 
-    path = "/Users/michael.tian/Desktop/BME 547/class_repos" \
-           "/final-project-spooky-dookie/images/"
-    images = ["acl1.jpg", "acl2.jpg", "esophagus 1.jpg", "esophagus2.jpg",
-              "synpic50411.jpg", "synpic51041.jpg", "synpic51042.jpg",
-              "upj1.jpg", "upj2.jpg", "test_ECG.jpg", "test_tachycardia.jpg"]
-    full_path = []
-    for i in range(len(images)):
-        full_path.append(path + images[i])
-    b64_images = []
-    for i in full_path:
-        b64_images.append(read_file_as_b64(i))
-    datetimes = ["2020-03-00 11:00:36", "2020-03-01 11:00:36",
-                 "2020-03-02 11:00:36"]
-    patient1 = Patient("Yume Choi", 3, ["acl1.png"], [b64_images[6]],
-                       [images[9]], [b64_images[-2]], [85], [datetimes[0]])
-    patient2 = Patient("Michael Tian", 5, images[0:9], b64_images[0:9],
-                       images[9:], b64_images[9:], [85, 90], datetimes[1:])
-    patient3 = Patient("Phoebe Dijour", 11, ["acl1.png", "acl2.png"],
-                       [b64_images[2], b64_images[3]], [images[10]],
-                       [b64_images[-1]], [85], [datetimes[0]])
-    patient1.save()
-    patient2.save()
-    patient3.save()
+    # path = "/Users/michael.tian/Desktop/BME 547/class_repos" \
+    #        "/final-project-spooky-dookie/images/"
+    # images = ["acl1.jpg", "acl2.jpg", "esophagus 1.jpg", "esophagus2.jpg",
+    #           "synpic50411.jpg", "synpic51041.jpg", "synpic51042.jpg",
+    #           "upj1.jpg", "upj2.jpg", "test_ECG.jpg", "test_tachycardia.jpg"]
+    # full_path = []
+    # for i in range(len(images)):
+    #     full_path.append(path + images[i])
+    # b64_images = []
+    # for i in full_path:
+    #     b64_images.append(read_file_as_b64(i))
+    # datetimes = ["2020-03-00 11:00:36", "2020-03-01 11:00:36",
+    #              "2020-03-02 11:00:36"]
+    # patient1 = Patient("Yume Choi", 3, ["acl1.png"], [b64_images[6]],
+    #                    [images[9]], [b64_images[-2]], [85], [datetimes[0]])
+    # patient2 = Patient("Michael Tian", 5, images[0:9], b64_images[0:9],
+    #                    images[9:], b64_images[9:], [85, 90], datetimes[1:])
+    # patient3 = Patient("Phoebe Dijour", 11, ["acl1.png", "acl2.png"],
+    #                    [b64_images[2], b64_images[3]], [images[10]],
+    #                    [b64_images[-1]], [85], [datetimes[0]])
+    # patient1.save()
+    # patient2.save()
+    # patient3.save()
 
     app.run()
