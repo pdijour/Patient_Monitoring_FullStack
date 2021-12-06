7ae80a368679ce94ccf924c51aba63a97eb032a8
Michael Tian <Michaeltian77@gmail.com>
2021-12-05 20:33:54 -0500

    Wrote process_b64 function to make code more modular, fixed some docstrings

--- cloud_server.py
+++ cloud_server.py
@@ -8,8 +8,6 @@ from database_definitions import Patient
 import base64
 import io
 import matplotlib.image as mpimg
-from matplotlib import pyplot as plt
-from skimage.io import imsave
 from skimage.transform import resize
 import numpy as np
 
@@ -99,6 +97,11 @@ def get_info():
     information is transferred to that dictionary under appropriate
     fields. This dictionary is then jsonified and returned for easy
     communication.
+
+    Returns
+    -------
+    Dict
+        Containing all the information about the selected patient
     """
     in_data = request.get_json()
     info = retrieve_all_info(in_data)
@@ -437,31 +440,29 @@ def resize_image(nd):
     return resized_nd
 
 
+def process_b64(b64):
+    """Converts a b64 string into a resized ndarray for display
+
+       This function takes in a b64 strings representing an image,
+       converts it to an ndarray using the b64_to_ndarray function,
+       then resized it to 250x250 using the resize_image function.
+       This resized ndarray is then returned to be displayed.
+
+       Parameters
+        ----------
+        b64 : str
+            A b64 string representing an image
+
+       Returns
+       -------
+       array
+           Containing the ndarray representing the resized image
+       """
+    nd = b64_to_ndarray(b64)
+    resized_medical_nd = resize_image(nd)
+    return resized_medical_nd
+
+
 if __name__ == '__main__':
     initialize_server()
-
-    # path = "/Users/michael.tian/Desktop/BME 547/class_repos" \
-    #        "/final-project-spooky-dookie/images/"
-    # images = ["acl1.jpg", "acl2.jpg", "esophagus 1.jpg", "esophagus2.jpg",
-    #           "synpic50411.jpg", "synpic51041.jpg", "synpic51042.jpg",
-    #           "upj1.jpg", "upj2.jpg", "test_ECG.jpg", "test_tachycardia.jpg"]
-    # full_path = []
-    # for i in range(len(images)):
-    #     full_path.append(path + images[i])
-    # b64_images = []
-    # for i in full_path:
-    #     b64_images.append(read_file_as_b64(i))
-    # datetimes = ["2020-03-00 11:00:36", "2020-03-01 11:00:36",
-    #              "2020-03-02 11:00:36"]
-    # patient1 = Patient("Yume Choi", 3, ["acl1.png"], [b64_images[6]],
-    #                    [images[9]], [b64_images[-2]], [85], [datetimes[0]])
-    # patient2 = Patient("Michael Tian", 5, images[0:9], b64_images[0:9],
-    #                    images[9:], b64_images[9:], [85, 90], datetimes[1:])
-    # patient3 = Patient("Phoebe Dijour", 11, ["acl1.png", "acl2.png"],
-    #                    [b64_images[2], b64_images[3]], [images[10]],
-    #                    [b64_images[-1]], [85], [datetimes[0]])
-    # patient1.save()
-    # patient2.save()
-    # patient3.save()
-
     app.run()
